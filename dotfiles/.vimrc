2f0f2720286d5f358920cb8fe8b8aac2233d47b0
Mai Huy Hoang <maihuyhoang97@gmail.com>
2020-09-15 20:55:26 +0700

    Update scripts and .vimrc

--- dotfiles/.vimrc
+++ dotfiles/.vimrc
@@ -1,5 +1,7 @@
 set runtimepath+=~/.vim_runtime
 set number relativenumber
+set clipboard=unnamed
+set clipboard=unnamedplus
 
 source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
--- scripts/post_install_ubuntu.sh
+++ scripts/post_install_ubuntu.sh
@@ -4,14 +4,18 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install git g++ python3 ubuntu-restricted-extras python3-pip vim snapd ibus fzf zsh xclip
+sudo apt install -y git g++ python3 ubuntu-restricted-extras python3-pip vim snapd ibus fzf zsh xclip curl fonts-hack
 
 # Fix time difference dual boot with Windows
 timedatectl set-local-rtc 1 --adjust-system-clock
 
-echo "Installing drivers"
-sudo ubuntu-drivers autoinstall
+echo "Installing Google Chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/Downloads
+sudo apt install -y ~/Downloads/google-chrome-stable_current_amd64.deb
+rm ~/Downloads/google-chrome-stable_current_amd64.deb
 
+# echo "Installing drivers"
+# sudo ubuntu-drivers autoinstall
 
 echo "Installing Calibre"
 sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
@@ -20,34 +24,30 @@ echo "Installing Spotify"
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
+sudo apt-get update && sudo apt-get install -y spotify-client
 
 echo "Installing WPS Office"
 wget http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/9615/wps-office_11.1.0.9615.XA_amd64.deb -P ~/Downloads
-sudo apt install ~/Downloads/wps-office_11.1.0.9615.XA_amd64.deb
+sudo apt install -y ~/Downloads/wps-office_11.1.0.9615.XA_amd64.deb
 rm -f ~/Downloads/wps-office_11.1.0.9615.XA_amd64.deb
 
 echo "Installing ibus-bamboo"
 sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
 sudo apt-get update
-sudo apt-get install ibus-bamboo
+sudo apt-get install -y ibus-bamboo
 ibus restart
 
-echo "Installing Google Chrome"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/Downloads
-sudo apt install ~/Downloads/google-chrome-stable_current_amd64.deb
-rm ~/Downloads/google-chrome-stable_current_amd64.deb
-
 echo "Installing Skype"
-wget https://repo.skype.com/latest/skypeforlinux-64.deb -p ~/Downloads
-sudo apt install ~/Downloads/skypeforlinux-64.deb
+wget https://repo.skype.com/latest/skypeforlinux-64.deb -P ~/Downloads
+sudo apt install -y ~/Downloads/skypeforlinux-64.deb
 rm ~/Downloads/skypeforlinux-64.deb
 
 echo "Installing Telegram"
-sudo apt install telegram-desktop
+sudo apt install -y telegram-desktop
 
 echo "Installing VSCode"
 wget https://update.code.visualstudio.com/latest/linux-deb-x64/stable -O ~/Downloads/vscode-amd64.deb
-sudo apt install ~/Downloads/vscode-amd64.deb
+sudo apt install -y ~/Downloads/vscode-amd64.deb
 rm ~/Downloads/vscode-amd64.deb
+
 
